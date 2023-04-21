7c42b1724e2cea17254e85eb30d21bc4c0ae5c97
ricacostt <ricacostt@gmail.com>
2023-04-22 00:14:19 +0100

    fixed line height

--- my-app/components/Bio/Bio.js
+++ my-app/components/Bio/Bio.js
@@ -5,13 +5,13 @@ function Bio() {
   return (
     <>
     <div className='flex flex-col justify-center max-w-screen-3xl ml-auto mr-auto pt-16 md:pt-32 xsm:pl-2 xsm:pr-2 pr-8 pl-8 gap-6'>
-        <div className='text-xl xsm:text-3xl text-textPrimaryLight dark:text-textPrimaryDark'>
+        <div className='text-xl xsm:text-3xl text-textPrimaryLight dark:text-textPrimaryDark xsm:leading-normal leading-8'>
             👋🏻 Hi, I’m Andrea Lead and Mentor Product Designer currently working at Deel.
             
             </div>
             <div className='text-textSecondaryLight dark:text-textSecondaryDark text-base xsm:text-xl'>Previously, founding designer at Form3, Leader designer at Grabyo. Also helped in creating internal tools for Facebook, Comcast, Twitter and many more.
             </div>
-            <div className='text-textSecondaryLight dark:text-textSecondaryDark text-base xsm:text-xl'>
+            <div className='text-textSecondaryLight dark:text-textSecondaryDark text-base xsm:text-xl xsm:leading-normal leading-8'>
             From <span className='underline'>Salento</span>, UK based.
         </div>
     </div>
