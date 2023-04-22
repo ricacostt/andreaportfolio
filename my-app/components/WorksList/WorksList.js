104a134056e9d9bcc00623f7e93c65e3b0eebacd
ricacostt <ricacostt@gmail.com>
2023-04-22 11:40:35 +0100

    add grids in work section

--- my-app/components/WorksList/WorksList.js
+++ my-app/components/WorksList/WorksList.js
@@ -8,33 +8,46 @@ export default function WorksList() {
       <h1 className="text-base xsm:text-xl font-medium text-textPrimaryLight dark:text-textPrimaryDark">
         Selected work
       </h1>
-      <div className="flex flex-row gap-32">
-        <Work
-          work={"Slack plugins - Web development"}
-          company={"tryroots.io"}
-        />
-
-        {/* <div className="pt-8 text-base w-80 ">
-          <div className="text-textPrimaryLight dark:text-textPrimaryDark">
-            New HRIS console - Web application
+      <div className="parent">
+        <div className="childBoss">
+          <div className=" work pt-8 text-base text-textPrimaryLight dark:text-textPrimaryDark">
+            Slack plugins - Web development{" "}
           </div>
-          <div className="text-textCompanyLight dark:text-textCompanyDark">
-            @ Deel
+          <div className="company text-textCompanyLight dark:text-textCompanyDark">
+            @ tryroots.io
           </div>
           <div className="flex flex-row gap-1 text-textPrimaryLight dark:text-textPrimaryDark pt-2">
-            <div className="flex">Coming soon</div>
+            <div className="flex">Learn more</div>
+            <div className="flex items-center ">
+              <HiArrowRight />
+            </div>
           </div>
-        </div> */}
+        </div>
 
-        <Work
-          work={"Slack plugins - Web development"}
-          company={"tryroots.io"}
-        />
-         <Work
-          work={"Slack plugins - Web development"}
-          company={"tryroots.io"}
-        />
-     
+        <div className="childBoss">
+          <div className=" work pt-8 text-base text-textPrimaryLight dark:text-textPrimaryDark">
+            New HRIS console - Web application{" "}
+          </div>
+          <div className="company text-textCompanyLight dark:text-textCompanyDark">
+            @ Deel
+          </div>
+          <div className="">Coming soon</div>
+        </div>
+
+        <div className="childBoss">
+          <div className=" work pt-8 text-base text-textPrimaryLight dark:text-textPrimaryDark">
+            Slack plugins - Web development{" "}
+          </div>
+          <div className="company text-textCompanyLight dark:text-textCompanyDark">
+            @ tryroots.io
+          </div>
+          <div className="flex flex-row gap-1 text-textPrimaryLight dark:text-textPrimaryDark pt-2">
+            <div className="flex">Learn more</div>
+            <div className="flex items-center ">
+              <HiArrowRight />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
