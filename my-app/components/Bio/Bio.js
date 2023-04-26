87cb6564f86b160158ad6065d87919e0b8d58623
Andrea <ilsabato@Andreas-MacBook-Pro.local>
2023-04-26 16:10:36 +0200

    Change to the links

--- my-app/components/Bio/Bio.js
+++ my-app/components/Bio/Bio.js
@@ -7,7 +7,7 @@ function Bio() {
     <>
     <div className='flex flex-col justify-center max-w-screen-3xl ml-auto mr-auto pt-16 md:pt-32 xsm:px-2 px-8 gap-6'>
         <div className='text-xl xsm:text-3xl text-textPrimaryLight dark:text-textPrimaryDark xsm:leading-normal leading-8'>
-            👋🏻 Hi, I’m Andrea Lead and Mentor Product Designer currently working at Deel.
+            👋🏻 Hi, I’m Andrea Lead and Mentor Product Designer currently working at <span className='underline'><a href="https://www.deel.com/" target="_blank" rel="noopener noreferrer">Deel</a></span>.
             </div>
             <div className='text-textSecondaryLight font-light dark:text-textSecondaryDark text-base xsm:text-xl'>Previously, founding designer at Form3, Leader designer at Grabyo. Also helped in creating internal tools for Meta (ex Facebook), Comcast, Twitter and many more.
             </div>
--- my-app/components/Work/Work.js
+++ my-app/components/Work/Work.js
@@ -6,14 +6,14 @@ function Work({ work, company, category, status, link, project }) {
     <>
    <div className="childBoss">
           <div className="work text-textCategoryLight font-normal dark:text-textCategoryDark">
-            {work} <a href={link} target="_blank" rel="noopener noreferrer">{company}</a> 
+            {work} <span className=''>@ </span><span className='underline'><a href={link} target="_blank" rel="noopener noreferrer">{company}</a></span> 
           </div>
           <div className="work text-textCategoryLight font-light text-sm dark:text-textCategoryDark">
             {category}
           </div>
           <div className="flex flex-row gap-1 text-textPrimaryLight font-medium dark:text-textPrimaryDark pt-2">
             <div className="flex"> <a href={project} target="_blank" rel="noopener noreferrer">{status}</a></div>
-            {status === 'Learn more' ?  <div className="flex items-center ">
+            {status === 'Case study' ?  <div className="flex items-center ">
               <HiArrowRight />
             </div> : null}
           </div>
