8dddca49523b2c8e7fc90500a2f70b57a430ba0f
Andrea <ilsabato@Andreas-MacBook-Pro.local>
2023-04-28 11:25:25 +0200

    Fixing grids

--- my-app/components/Bio/Bio.js
+++ my-app/components/Bio/Bio.js
@@ -5,7 +5,7 @@ import Work from '../WorksList/WorksList'
 function Bio() {
   return (
     <>
-    <div className='flex flex-col justify-center max-w-screen-3xl ml-auto mr-auto pt-16 md:pt-32 xsm:px-2 px-8 gap-6'>
+    <div className='flex flex-col justify-center max-w-screen-3xl ml-auto mr-auto pt-16 md:pt-32 xsm:px-8 px-8 gap-6'>
         <div className='text-xl xsm:text-3xl text-textPrimaryLight dark:text-textPrimaryDark xsm:leading-normal leading-8'>
             👋🏻 Hi, I’m Andrea Lead and Mentor Product Designer currently working at <span className='underline'><a href="https://www.deel.com/" target="_blank" rel="noopener noreferrer">Deel</a></span>.
             </div>
@@ -15,11 +15,11 @@ function Bio() {
             From <span className='underline'><a href="https://en.wikipedia.org/wiki/Salento" target="_blank" rel="noopener noreferrer">Salento</a></span>, UK based, working remotely.
         </div>
     </div>
-    <div className='flex flex-row flex-wrap gap-4 justify-start ml-auto mr-auto pt-12 xsm:px-2 px-8'>
-        <Button href={''} text={'LINKEDIN'}/>
-        <Button href={''} text={'BEHANCE'}/>
-        <Button href={''} text={'RESUME'}/>
-        <Button href={''} text={'CONTACT ME'}/>
+    <div className='flex flex-row flex-wrap gap-4 justify-start ml-auto mr-auto pt-12 xsm:px-8 px-8'>
+        <Button href={'https://www.linkedin.com/in/ilsabato/'} text={'LINKEDIN'}/>
+        <Button href={'https://www.behance.net/ilsab'} text={'BEHANCE'}/>
+        <Button href={'https://www.dropbox.com/s/77f4lqbicc2wtl1/andreasabato-cv.pdf?dl=0'} text={'RESUME'}/>
+        <Button href={'mailto:ilsabato@gmail.com'} text={'CONTACT ME'}/>
     </div>
     <Work/>
 </>
--- my-app/components/WorksList/WorksList.js
+++ my-app/components/WorksList/WorksList.js
@@ -4,7 +4,7 @@ import { HiArrowRight } from "react-icons/hi";
 
 export default function WorksList() {
   return (
-    <div className="pt-16 ms:pt-32 xsm:px-2 px-8 pb-24">
+    <div className="pt-16 ms:pt-32 xsm:px-8 px-8 pb-24">
       <h1 className="text-base xsm:text-xl font- pb-8 text-textPrimaryLight dark:text-textPrimaryDark">
         Selected work
       </h1>
