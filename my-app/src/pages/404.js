106cabb8d3892ca40b11962824b952bd307958c3
ricacostt <ricacostt@gmail.com>
2023-05-06 20:24:59 +0100

    add link to go back home

--- my-app/src/pages/404.js
+++ my-app/src/pages/404.js
@@ -1,3 +1,15 @@
+import Link from 'next/link'
 export default function Custom404() {
-    return <h1>404 - Page Not Found</h1>;
-  }
+  return (
+    <>
+      <h1>404 - Page Not Found</h1>
+      <button>
+      <Link href="/">
+   
+        Go back home
+     
+    </Link>
+    </button>
+    </>
+  );
+}
