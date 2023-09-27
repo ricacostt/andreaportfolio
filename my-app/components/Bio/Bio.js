4f6a783d62efd0c5539abd68d67a6668b93ccbcf
Andrea <ilsabato@Andreas-MacBook-Pro.local>
2023-09-27 13:45:52 +0200

    New Job title in Bio

--- my-app/components/Bio/Bio.js
+++ my-app/components/Bio/Bio.js
@@ -7,9 +7,9 @@ function Bio() {
     <>
     <div className='flex flex-col justify-center max-w-screen-3xl ml-auto mr-auto pt-16 md:pt-32 xsm:px-8 px-8 gap-6'>
         <div className='text-xl xsm:text-3xl text-textPrimaryLight dark:text-textPrimaryDark xsm:leading-normal leading-8'>
-            👋🏻 Hi, I’m Andrea Lead and Mentor Product Designer currently working at <span className='underline'><a href="https://www.deel.com/" target="_blank" rel="noopener noreferrer">Deel</a></span>.
+            👋🏻 Hi, I’m Andrea Lead and Mentor Product Designer currently working at <span className='underline'><a href="https://flair.hr/en/" target="_blank" rel="noopener noreferrer">flair</a></span>.
             </div>
-            <div className='text-textSecondaryLight font-light dark:text-textSecondaryDark text-base xsm:text-xl'>Previously, founding designer at Form3, Leader designer at Grabyo. Also helped in creating internal tools for Meta (ex Facebook), Comcast, Twitter and many more.
+            <div className='text-textSecondaryLight font-light dark:text-textSecondaryDark text-base xsm:text-xl'>Previously, Product Designer at Deel, founding designer at Form3, Leader designer at Grabyo. Also helped in creating internal tools for Meta (ex Facebook), Comcast, Twitter and many more.
             </div>
             <div className='text-textSecondaryLight font-light dark:text-textSecondaryDark text-base xsm:text-xl xsm:leading-normal leading-8'>
             From <span className='underline'><a href="https://en.wikipedia.org/wiki/Salento" target="_blank" rel="noopener noreferrer">Salento</a></span>, UK based, working remotely.
